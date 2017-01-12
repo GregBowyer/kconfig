78c20c4b10a3fc18a68193ecc6681f49b4c0f481
Greg Bowyer <gbowyer@fastmail.co.uk>
2017-01-11 16:35:56 -0800

    [Build] Remove libintl support

--- lxdialog/dialog.h
+++ lxdialog/dialog.h
@@ -26,11 +26,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-#ifndef KBUILD_NO_NLS
-# include <libintl.h>
-#else
-# define gettext(Msgid) ((const char *) (Msgid))
-#endif
+#define gettext(Msgid) ((const char *) (Msgid))
 
 #ifdef __sun__
 #define CURS_MACROS
